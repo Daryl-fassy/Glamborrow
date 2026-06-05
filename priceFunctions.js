07a5bf271b7241f92e74ae78ff0d6efd2eac83cf
Daryl <dakalonkhumbuleni@gmail.com>
2026-06-05 16:16:13 +0200

    updated the prices

--- priceFunctions.js
+++ priceFunctions.js
@@ -1,13 +1,13 @@
 export function RentalPrice(RentingPrice){
-  let halfPrice=RentingPrice*0.5;
-  let interest=RentingPrice*0.05;
+  let halfPrice=RentingPrice*0.4;
+  let interest=RentingPrice*0.02;
   let RentPriceTodisplay=halfPrice+interest;
 
  return RentPriceTodisplay;
 };
 
 export function BuyPrice(BuyingPrice){
-  let interest=BuyingPrice*0.15;
+  let interest=BuyingPrice*0.02;
   let BuyPriceToDisplay=BuyingPrice+interest;
   return BuyPriceToDisplay;
 }
